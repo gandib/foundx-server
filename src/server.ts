d0271885c440b8a5ee148e3a2b60efbf1f55a36e
gandib <gandibroy11@gmail.com>
2024-09-23 19:21:22 +0600

    branch completed added

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,10 @@ process.on('uncaughtException', (error) => {
   console.error('Uncaught Exception:', error);
   process.exit(1);
 });
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  process.exit(1);
+});
 
 process.on('unhandledRejection', (error) => {
   console.error('Unhandled Rejection:', error);
